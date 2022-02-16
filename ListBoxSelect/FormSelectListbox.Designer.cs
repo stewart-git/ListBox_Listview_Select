0d3831be6f27d60c2eea64e18d8cccdb66abf5dc
stewart-git <Stewart.Godwin@smtafe.wa.edu.au>
2022-02-16 11:19:55 +0800

    Add project files.

---
+++ ListBoxSelect/FormSelectListbox.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ListBoxSelect
+{
+    partial class FormSelectListbox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxItems = new System.Windows.Forms.ListBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.listViewItems = new System.Windows.Forms.ListView();
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSalary = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelListBox = new System.Windows.Forms.Label();
+            this.labelListView = new System.Windows.Forms.Label();
+            this.labelTextBoxName = new System.Windows.Forms.Label();
+            this.labelTextBoxSalary = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxItems
+            // 
+            this.listBoxItems.FormattingEnabled = true;
+            this.listBoxItems.Location = new System.Drawing.Point(13, 32);
+            this.listBoxItems.Name = "listBoxItems";
+            this.listBoxItems.Size = new System.Drawing.Size(134, 160);
+            this.listBoxItems.TabIndex = 0;
+            this.listBoxItems.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBoxItems_MouseClick);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(163, 71);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.ReadOnly = true;
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.Location = new System.Drawing.Point(163, 110);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.ReadOnly = true;
+            this.textBoxSalary.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSalary.TabIndex = 2;
+            // 
+            // listViewItems
+            // 
+            this.listViewItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderSalary});
+            this.listViewItems.HideSelection = false;
+            this.listViewItems.Location = new System.Drawing.Point(284, 32);
+            this.listViewItems.Name = "listViewItems";
+            this.listViewItems.Size = new System.Drawing.Size(145, 157);
+            this.listViewItems.TabIndex = 3;
+            this.listViewItems.UseCompatibleStateImageBehavior = false;
+            this.listViewItems.View = System.Windows.Forms.View.Details;
+            this.listViewItems.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listViewItems_MouseClick);
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "Name";
+            this.columnHeaderName.Width = 80;
+            // 
+            // columnHeaderSalary
+            // 
+            this.columnHeaderSalary.Text = "Salary";
+            // 
+            // labelListBox
+            // 
+            this.labelListBox.AutoSize = true;
+            this.labelListBox.Location = new System.Drawing.Point(10, 16);
+            this.labelListBox.Name = "labelListBox";
+            this.labelListBox.Size = new System.Drawing.Size(44, 13);
+            this.labelListBox.TabIndex = 4;
+            this.labelListBox.Text = "List Box";
+            // 
+            // labelListView
+            // 
+            this.labelListView.AutoSize = true;
+            this.labelListView.Location = new System.Drawing.Point(281, 16);
+            this.labelListView.Name = "labelListView";
+            this.labelListView.Size = new System.Drawing.Size(49, 13);
+            this.labelListView.TabIndex = 5;
+            this.labelListView.Text = "List View";
+            // 
+            // labelTextBoxName
+            // 
+            this.labelTextBoxName.AutoSize = true;
+            this.labelTextBoxName.Location = new System.Drawing.Point(160, 55);
+            this.labelTextBoxName.Name = "labelTextBoxName";
+            this.labelTextBoxName.Size = new System.Drawing.Size(80, 13);
+            this.labelTextBoxName.TabIndex = 6;
+            this.labelTextBoxName.Text = "Text Box Name";
+            // 
+            // labelTextBoxSalary
+            // 
+            this.labelTextBoxSalary.AutoSize = true;
+            this.labelTextBoxSalary.Location = new System.Drawing.Point(160, 94);
+            this.labelTextBoxSalary.Name = "labelTextBoxSalary";
+            this.labelTextBoxSalary.Size = new System.Drawing.Size(81, 13);
+            this.labelTextBoxSalary.TabIndex = 7;
+            this.labelTextBoxSalary.Text = "Text Box Salary";
+            // 
+            // FormSelectListbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 207);
+            this.Controls.Add(this.labelTextBoxSalary);
+            this.Controls.Add(this.labelTextBoxName);
+            this.Controls.Add(this.labelListView);
+            this.Controls.Add(this.labelListBox);
+            this.Controls.Add(this.listViewItems);
+            this.Controls.Add(this.textBoxSalary);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.listBoxItems);
+            this.Name = "FormSelectListbox";
+            this.Text = "Select Listbox Item";
+            this.Load += new System.EventHandler(this.FormSelectListbox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxItems;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.ListView listViewItems;
+        private System.Windows.Forms.Label labelListBox;
+        private System.Windows.Forms.Label labelListView;
+        private System.Windows.Forms.Label labelTextBoxName;
+        private System.Windows.Forms.Label labelTextBoxSalary;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderSalary;
+    }
+}
+
---
+++ ListBoxSelect/FormSelectListbox.cs
@@ -0,0 +1,101 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+// two methods for displaying arrays
+// method one uses a List Box
+// method two uses a List View
+// ListView: display as "Details"
+// and Add column text
+namespace ListBoxSelect
+{
+    public partial class FormSelectListbox : Form
+    {
+        public FormSelectListbox()
+        {
+            InitializeComponent();
+        }
+        // declare and fill two arrays of size seven
+        string[] arrayName = new string[] {"Red", "Orange", "Yellow", "Green", "Blue", "Indego", "Violet" };
+        int[] arraySalary = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        
+        //================ LIST BOX ================================
+        // select item in List Box and display in Text Box
+        private void listBoxItems_MouseClick(object sender, MouseEventArgs e)
+        {
+            // select a record in the Listbox
+            string curItem = listBoxItems.SelectedItem.ToString();
+            // find the index of the selected item
+            int indx = listBoxItems.FindString(curItem);
+            // put the Listbox item into the textbox
+            textBoxName.Text = arrayName[indx];
+            // convert integer into string
+            textBoxSalary.Text = arraySalary[indx].ToString();
+            // pass parameter value (integer) to method
+            ChangeTextBoxColour(indx);
+        }
+        // display arrays in List Box
+        private void DisplayArraysListBox()
+        {
+            // clear the list box
+            listBoxItems.Items.Clear();
+            for (int i = 0; i < arrayName.Length; i++)
+            {
+                // add items to Listbox - use a tab to seperate items
+                listBoxItems.Items.Add(arrayName[i] + "\t" + arraySalary[i]);
+            }
+        }
+
+        //================= LIST VIEW ================================
+        // select item in List View and display in Text Box
+        private void listViewItems_MouseClick(object sender, MouseEventArgs e)
+        {
+            int currentItem = listViewItems.SelectedIndices[0];
+            // put the selected List View item into the textbox
+            textBoxName.Text = arrayName[currentItem];
+            // convert integer into string
+            textBoxSalary.Text = arraySalary[currentItem].ToString();
+            // pass parameter value (integer) to method
+            ChangeTextBoxColour(currentItem);
+        }
+        // display arrays in List View
+        private void DisplayArraysListView()
+        {
+            listViewItems.Items.Clear();
+            for (int i = 0; i < arrayName.Length; i++)
+            {
+                // create a list view item with the first array
+                ListViewItem lvi = new ListViewItem(arrayName[i]);
+                // add the next list view item from the second array
+                lvi.SubItems.Add(arraySalary[i].ToString());
+                // add the list view items to the List View
+                listViewItems.Items.Add(lvi);
+            }
+        }
+        //==================================================
+        // method to change text box colours 
+        // receives paramater from method call (integer)
+        private void ChangeTextBoxColour(int colour)
+        {
+            textBoxName.ForeColor = Color.Black;
+            switch (colour)
+            {
+                case 0: textBoxName.BackColor = Color.Red; break;
+                case 1: textBoxName.BackColor = Color.Orange; break;
+                case 2: textBoxName.BackColor = Color.Yellow; break;
+                case 3: textBoxName.BackColor = Color.Green; break;
+                case 4: textBoxName.BackColor = Color.Blue;
+                        textBoxName.ForeColor = Color.White; break;
+                case 5: textBoxName.BackColor = Color.Indigo;
+                        textBoxName.ForeColor = Color.White; break;
+                case 6: textBoxName.BackColor = Color.Violet; break;
+            }
+        }
+
+        private void FormSelectListbox_Load(object sender, System.EventArgs e)
+        {
+            // display the arrays in the List Box when the form is opened
+            DisplayArraysListBox();
+            // display the arrays in the List View when the form is opened
+            DisplayArraysListView();
+        }
+    }
+}
